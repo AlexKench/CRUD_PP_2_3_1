9541e10ee28e9f3f1f31828651ab84617883cc6b
Alexander <sasha.maks92@gmail.com>
2023-04-08 20:07:46 +0300

    Initial commit

---
+++ src/main/java/com/alexander/maksimov/controllers/UserController.java
@@ -0,0 +1,63 @@
+package com.alexander.maksimov.controllers;
+
+import com.alexander.maksimov.models.User;
+import com.alexander.maksimov.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.findOne(id));
+        return "show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+
+        userService.save(user);
+        return "redirect:/user";
+    }
+
+    @GetMapping("/{id}/update")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.findOne(id));
+        return "update";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.update(id, user);
+        return "redirect:/user";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/user";
+    }
+}
---
+++ src/main/java/com/alexander/maksimov/services/UserService.java
@@ -0,0 +1,45 @@
+package com.alexander.maksimov.services;
+
+import com.alexander.maksimov.models.User;
+import com.alexander.maksimov.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findOne(int id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(int id, User user) {
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+}
